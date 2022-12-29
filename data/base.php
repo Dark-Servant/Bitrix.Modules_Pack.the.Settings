0eb0a04d8589433a5895d6bedd69ec777c338951
Konstantin Beloglazov <desiresofgod@gmail.com>
2022-12-29 14:11:39 +0500

    В базовый класс PackTheSettings\Data\Base добавлена возможность создания методов индивидуальных для объекта класса

--- data/base.php
+++ data/base.php
@@ -1,14 +1,23 @@
 <?
 namespace PackTheSettings\Data;
 
+use \PackTheSettings\Arguments\ClassName;
+
 abstract class Base implements IBase
 {
     protected $params = [];
     protected $errorText = '';
+    protected $paramMethodData = [];
+
+    public function getParamMethodNames(): array
+    {
+        return array_keys($this->paramMethodData);
+    }
 
     public function setParams(array $params): IBase
     {
         $this->errorText = '';
+        $this->paramMethodData = [];
         $this->params = $params;
         return $this;
     }
@@ -21,10 +30,12 @@ abstract class Base implements IBase
     public function getChangedParams(): array
     {
         $defaultValues = static::getDefaultParams();
+        
         return array_filter(
                     $this->params,
                     function($value, $key) use($defaultValues) {
-                        return isset($defaultValues[$key]) && ($value != $defaultValues[$key]);
+                        return isset($defaultValues[$key]) && !is_object($value) &&
+                               !is_array($value) && ($value != $defaultValues[$key]);
                     },
                     ARRAY_FILTER_USE_BOTH
                 );
@@ -33,5 +44,44 @@ abstract class Base implements IBase
     public function getNormalizedParams(): array
     {
         return $this->getMainParams() + $this->getChangedParams() + static::getDefaultParams();
+    }
+
+    protected function initParamMethod(string $name, array $filter, string $className = self::class)
+    {
+        $realClassName = ClassName::getLastRelative(self::class, $className);
+        $this->paramMethodData[$name]['callable'] = function() use($name, $realClassName, $filter) {
+            if (!empty($this->paramMethodData[$name]['result']))
+                return $this->paramMethodData[$name]['result'];
+            
+            $result = $realClassName::get($filter);
+            if (!isset($result)) {
+                $this->errorText = Loc::getMessage(
+                                        'ERROR_BAD_LINKED_UNIT_FILTER',
+                                        [
+                                            '#NAME#' => $name,
+                                            '#CLASSNAME#' => $realClassName,
+                                            '#FILTER#' => PHP_EOL . print_r($filter, true),
+                                        ]
+                                    );
+    
+            } else {
+                $this->paramMethodData[$name]['result'] = $result;
+            }
+            return $this->paramMethodData[$name]['result'];
+        };
+    }
+    
+    protected function getParamMethodData(string $methodName)
+    {
+        $name = strtolower($methodName);
+        return is_callable($this->paramMethodData[$name]['callable'])
+             ? $this->paramMethodData[$name]['callable']()
+             : null;
+    }
+
+    public function __call(string $methodName, array $params = [])
+    {
+        if (preg_match('/^get(\w*)/i', $methodName, $methodNameParts))
+            return $this->getParamMethodData($methodNameParts[1]);
     }
 }
---
+++ lang/ru/data/base.php
@@ -0,0 +1,2 @@
+<?
+$MESS['ERROR_BAD_LINKED_UNIT_FILTER'] = 'Для временного метода #NAME# не удалось получить данные из класса #CLASSNAME# по фильтру #FILTER#';
